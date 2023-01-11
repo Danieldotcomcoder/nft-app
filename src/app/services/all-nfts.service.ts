67fce706769415bd45afaa23989176eab7e6ae34
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-11 09:58:21 +0200

    Filter nfts to show only with metadata

--- src/app/services/all-nfts.service.ts
+++ src/app/services/all-nfts.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Root } from '../models/nfts.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AllNftsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getallnfts(): Observable<Root> {
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'X-API-KEY': 'iu87qb1bkTsbBD5bZdFqDYcCRTc8PxVe'}
+    };
+    
+    return this.http.get<Root>('https://api.blockspan.com/v1/nfts?chain=eth-main&page_size=100', options)
+  
+  }
 }
